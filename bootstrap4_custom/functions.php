6d0900f6d036769690af2abb457fb594bc66cee2
Cédric Andrietti <cedric.andrietti@gmail.com>
2019-04-12 01:52:24 +0200

    correct path

--- bootstrap4_custom/functions.php
+++ bootstrap4_custom/functions.php
@@ -2,8 +2,8 @@
 
 // Folders
 define('THEME_DIR', get_template_directory());
-define('THEME_JS', THEME_DIR . '/js');
-define('THEME_CSS', THEME_DIR . '/css');
+define('THEME_JS', THEME_DIR . '/dist/js');
+define('THEME_CSS', THEME_DIR . '/dist/css');
 define('THEME_IMAGES', THEME_DIR . '/img');
 define('THEME_INCLUDES', THEME_DIR . '/inc');
 define('THEME_TEMPLATES', THEME_DIR . '/templates');
