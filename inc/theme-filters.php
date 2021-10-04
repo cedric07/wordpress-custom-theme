51c81c696ce1bef440cbbd6432f7480455eb371f
cedric07 <cedric.andrietti@gmail.com>
2021-10-04 18:06:12 +0200

    Correction ACF deprecated

--- inc/theme-filters.php
+++ inc/theme-filters.php
@@ -4,4 +4,4 @@
 add_filter( 'excerpt_more', 'view_more_article' ); // Add 'View Article' button instead of [...] for Excerpts
 
 // ACF
-add_filter( 'block_categories', 'my_acf_block_categories', 10, 2 ); // ACF Create block categories
+add_filter( 'block_categories_all', 'my_acf_block_categories', 10, 2 ); // ACF Create block categories
