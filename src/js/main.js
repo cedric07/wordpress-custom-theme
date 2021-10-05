e329c92b04ff6528d7f9972bec6ae60bcb1b18c7
cedric07 <cedric.andrietti@gmail.com>
2021-10-05 18:35:22 +0200

    Change breakpoint for bootstrap 5

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,16 @@ var APP = APP || {};
 var $ = jQuery.noConflict();
 
 APP.pref = {
+	screen_xxl: 1400,
+	screen_xxl_down: 1399.98,
 	screen_xl: 1200,
-	screen_lg_down: 1199.98,
+	screen_xl_down: 1199.98,
 	screen_lg: 992,
-	screen_md_down: 991.98,
+	screen_lg_down: 991.98,
 	screen_md: 768,
-	screen_sm_down: 767.98,
+	screen_md_down: 767.98,
 	screen_sm: 576,
-	screen_xs_down: 575.98
+	screen_sm_down: 575.98
 };
 
 $(document).ready(function () {
