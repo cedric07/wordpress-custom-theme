afb2458349c4ac755e01c2e570d6ee6eae79839a
cedric07 <cedric.andrietti@gmail.com>
2021-12-15 18:07:26 +0100

    disable auto updates

--- inc/theme-filters.php
+++ inc/theme-filters.php
@@ -6,3 +6,8 @@ add_filter( 'excerpt_more', 'view_more_article' ); // Add 'View Article' button 
 // ACF
 add_filter( 'block_categories_all', 'my_acf_block_categories', 10, 2 ); // ACF Create block categories
 add_filter( 'allowed_block_types_all', 'my_acf_allowed_blocks', 10, 2 ); // ACF allowed blocks
+
+// Auto update
+add_filter( 'plugins_auto_update_enabled', '__return_false' ); // Disable auto update plugins
+add_filter( 'themes_auto_update_enabled', '__return_false' ); // Disable auto update themes
+add_filter( 'allow_major_auto_core_updates', '__return_false' ); // Disable auto update core major
