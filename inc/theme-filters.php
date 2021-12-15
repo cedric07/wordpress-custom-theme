b4cc002ba29bf1a9a7931c518c1a4761a06fcf59
cedric07 <cedric.andrietti@gmail.com>
2021-12-15 18:32:11 +0100

    yoast functions

--- inc/theme-filters.php
+++ inc/theme-filters.php
@@ -11,3 +11,7 @@ add_filter( 'allowed_block_types_all', 'my_acf_allowed_blocks', 10, 2 ); // ACF 
 add_filter( 'plugins_auto_update_enabled', '__return_false' ); // Disable auto update plugins
 add_filter( 'themes_auto_update_enabled', '__return_false' ); // Disable auto update themes
 add_filter( 'allow_major_auto_core_updates', '__return_false' ); // Disable auto update core major
+
+// Yoast
+add_filter( 'wpseo_primary_term_taxonomies', '__return_empty_array' ); // Disabling the Primary category feature
+add_filter( 'wpseo_metabox_prio', 'move_yoast_below_acf' ); // Change Yoast SEO priority to 'low' to get ACF fields before Yoast metabox.
--- inc/theme-sub-filters.php
+++ inc/theme-sub-filters.php
@@ -12,3 +12,11 @@ function view_more_article( $more ) {
 
 	return '... <a class="view-article" href="' . get_permalink( $post->ID ) . '">' . __( 'View Article', 'your_text_domain' ) . '</a>';
 }
+
+/**
+ * Change Yoast SEO priority to 'low' to get ACF fields before Yoast metabox.
+ * @return string
+ */
+function move_yoast_below_acf() {
+	return 'low';
+}
